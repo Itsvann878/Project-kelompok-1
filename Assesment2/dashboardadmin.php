9adc7eecdb3ff809ff13c53cf78772115828a5d1
Firdho-03 <firdhoaulia@gmail.com>
2026-06-02 11:17:10 +0700

    santai
    santai

--- Assesment2/dashboardadmin.php
+++ Assesment2/dashboardadmin.php
@@ -9,3 +9,14 @@ require "inc.koneksi.php";
 echo "Welcome, <b>". $_SESSION["name"]."</b><br>";
 echo "Anda login sebagai, <b>". $_SESSION["role"]."</b>";
 ?>
+
+<div id="navbar3" class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+        <li><a href="dashboardadmin.php">Home</a></li>
+        <li><a href="dashboardadmin.php?Pages=employeelist">Employee List</a></li>
+        <li><a href="dashboardadmin.php?Pages=departmentlist">Department List</a></li>
+        <li><a href="dashboardadmin.php?Pages=userlist">User List</a></li>
+        <li><a href="dashboardadmin.php?Pages=projectlist">Project List</a></li>
+        <li><a href="dashboardadmin.php?=logout">Logout</a></li>
+    </ul>
+</div>
