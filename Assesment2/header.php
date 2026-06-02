2f828ff562a961c4116dda8d1382de22abd12879
ipan <irvanadr8@gmail.com>
2026-06-02 11:19:54 +0700

    Update header.php

--- Assesment2/header.php
+++ Assesment2/header.php
@@ -66,7 +66,7 @@
                 <li class="nav-item"><a class="nav-link" href="Main.php?Pages=employeelist">Employee List</a></li>
                 <li class="nav-item"><a class="nav-link" href="Main.php?Pages=employeeproject">Employee Project</a></li>
                 <li class="nav-item"><a class="nav-link" href="Main.php?Pages=register">Register</a></li>
-                <li class="nav-item"><a class="nav-link" href="Main.php?Pages=login">login</a></li>
+                <li class="nav-item"><a class="nav-link" href="Main.php?Pages=login">Login</a></li>
             </ul>
         </div>
         <a href="#" class="btn btn-info-tiket px-4">Info Tiket</a>
